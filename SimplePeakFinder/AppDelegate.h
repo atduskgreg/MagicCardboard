845abc18bfc33ad0d516bad582b7641d392a990a
Jessica <jessq@mit.edu>
2014-10-13 13:32:43 -0400

    Initial Commit

---
+++ SimplePeakFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimplePeakFinder
+//
+//  Created by Jessica Qian on 10/13/14.
+//  Copyright (c) 2014 Jessica Qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimplePeakFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimplePeakFinder
+//
+//  Created by Jessica Qian on 10/13/14.
+//  Copyright (c) 2014 Jessica Qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
