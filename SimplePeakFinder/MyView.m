b732d904215145b5453fe0ac227c66437472dbc9
Jessica <jessq@mit.edu>
2014-10-19 19:28:59 -0400

    added xPosition and boundingbox method

--- SimplePeakFinder/MyView.m
+++ SimplePeakFinder/MyView.m
@@ -10,6 +10,10 @@
 
 @interface MyView()
 @property (nonatomic) CGFloat averageYPosition;
+@property (nonatomic) CGFloat averageXPosition;
+@property (nonatomic) BOOL box1;
+@property (nonatomic) BOOL box2;
+
 @end
 
 @implementation MyView
@@ -57,13 +61,13 @@
 }
 
 
-- (BOOL)_processTouchesForPeek:(NSSet *)touches;
-{
+- (BOOL)_processTouchesForPeek:(NSSet *)touches {
     CGFloat sumOfYPositions = 0.0f;
+    CGFloat sumOfXPositions = 0.0f;
     BOOL largeTouchFound = NO;
-    
     for (UITouch *touch in touches) {
         sumOfYPositions += [touch locationInView:self].y;
+        sumOfXPositions += [touch locationInView:self].x;
         if ([touch respondsToSelector:@selector(majorRadius)]) {
             CGFloat touchSize = touch.majorRadius;
             if (touchSize > 10) {
@@ -74,23 +78,37 @@
     if (!largeTouchFound) {
         return NO;
     }
+    if ((float)[touches count] <= 1) {
+        return NO;
+    }
     self.averageYPosition = sumOfYPositions/(float)[touches count];
+    self.averageXPosition = sumOfXPositions/(float)[touches count];
+    self.box1 = [self _inBoundingBox:touches :160 :200 :0 :350];
+    self.box2 = [self _inBoundingBox:touches :380 :420 :0 :350];
+
     return YES;
 }
 
-//Draw a line at averageYPosition
+- (BOOL)_inBoundingBox:(NSSet *)touches :(int)top :(int)bottom :(int)left :(int)right;
+{
+    int count = 0;
+    for (UITouch *touch in touches) {
+        if ([touch locationInView:self].y < top && [touch locationInView:self].y > bottom){
+            if ([touch locationInView:self].x < right && [touch locationInView:self].x > left){
+                count += 1;
+            }
+        }
+    }
+    return (count > [touches count]/4);
+}
+
 - (void)drawRect:(CGRect)rect {
     [super drawRect:rect];
     CGContextRef context = UIGraphicsGetCurrentContext();
-    //Draw a UIBezier path
-    CGContextSetStrokeColorWithColor(context, [[UIColor blueColor] CGColor]);
-    UIBezierPath *blueHalf = [UIBezierPath bezierPath];
-    [blueHalf addArcWithCenter:CGPointMake(150, 200) radius:130.0 startAngle:M_PI+0.5 endAngle:-0.5 clockwise:YES];
-    [blueHalf setLineWidth:4.0];
-    [blueHalf stroke];
+    
     //Draw a card
-    CGRect rectangle = CGRectMake(25, 180, 250, 300);
-    if (self.averageYPosition > 50 && self.averageYPosition < 150) {
+    CGRect rectangle1 = CGRectMake(0, 0, 350, 180);
+    if (self.box1) {
         CGContextSetRGBFillColor(context, 0.0, 1.0, 0.0, 0.5);
         CGContextSetRGBStrokeColor(context, 0.0, 1.0, 0.0, 0.5);
     }
@@ -98,13 +116,32 @@
         CGContextSetRGBFillColor(context, 1.0, 0.0, 0.0, 0.5);
         CGContextSetRGBStrokeColor(context, 1.0, 0.0, 0.0, 0.5);
     }
-    CGContextFillRect(context, rectangle);
+    CGContextFillRect(context, rectangle1);
+    
+    //Draw a card
+    CGRect rectangle2 = CGRectMake(0, 400, 350, 180);
+    if (self.box2) {
+        CGContextSetRGBFillColor(context, 0.0, 1.0, 0.0, 0.5);
+        CGContextSetRGBStrokeColor(context, 0.0, 1.0, 0.0, 0.5);
+    }
+    else {
+        CGContextSetRGBFillColor(context, 1.0, 0.0, 0.0, 0.5);
+        CGContextSetRGBStrokeColor(context, 1.0, 0.0, 0.0, 0.5);
+    }
+    CGContextFillRect(context, rectangle2);
 
-    //Draw the yposition line
+    //Draw the yPosition line
     CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
     CGContextSetLineWidth(context, 2.0f);
     CGContextMoveToPoint(context, 0.0f, self.averageYPosition); //start at this point
     CGContextAddLineToPoint(context, 2000.0f, self.averageYPosition); //draw to this point
+    CGContextStrokePath(context);
+    
+    //Draw the xPosition line
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+    CGContextSetLineWidth(context, 2.0f);
+    CGContextMoveToPoint(context, self.averageXPosition, 0.0f); //start at this point
+    CGContextAddLineToPoint(context, self.averageXPosition, 2000.0f); //draw to this point
     CGContextStrokePath(context);
 }
 
